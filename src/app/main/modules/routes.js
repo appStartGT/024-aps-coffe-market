4fed383c2d5a7a6dd0742d9c7c70a0184886f530
Dary Castillo <darycastillo27@gmail.com>
2024-10-30 13:00:52 -0600

    fix imports

--- src/app/main/modules/routes.js
+++ src/app/main/modules/routes.js
@@ -1,10 +1,8 @@
 import { Navigate } from 'react-router-dom';
 import administratorRoutes from './administrator/routes';
-import employees from './employees/routes';
 import purchase from './purchase/routes';
 
 export default [
-  ...employees,
   ...administratorRoutes,
   ...purchase,
   { path: '*', to: 'purchase', element: Navigate },
